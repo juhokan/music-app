1f9446680417846ebc008863a0c990b61e3b7a72
Juho Kangas <juho.kangas@aalto.fi>
2024-07-16 17:41:54 +0300

    init playwright

---
+++ client/tests/app.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Music app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.locator('text=music review app')).toBeVisible()
+  })
+})
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3003",
+        target: "https://music-app-server.fly.dev",
         changeOrigin: true
       }
     }
