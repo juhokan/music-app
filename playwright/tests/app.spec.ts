19fe22532ea4094a6a227eec7612b3a694920e1e
Juho Kangas <juho.kangas@aalto.fi>
2024-07-16 16:27:42 +0300

    remove playwright

--- playwright/tests/app.spec.ts
+++
@@ -1,7 +0,0 @@
-const { test, describe, expect } = require('@playwright/test')
-
-describe('Music App', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('');
-  });
-})
