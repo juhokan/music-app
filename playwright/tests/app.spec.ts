4a08ef86d0e3834223f0f7900dd6bd2e128d37cb
Juho Kangas <juho.kangas@aalto.fi>
2024-07-16 15:04:45 +0300

    debugging

--- playwright/tests/app.spec.ts
+++ playwright/tests/app.spec.ts
@@ -2,7 +2,8 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Music App', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('')
-    await expect(page.getByText('music review app')).toBeVisible()
-  })
+    await page.goto('');
+    await page.waitForLoadState('networkidle');
+    await expect(page.getByText('music review app')).toBeVisible();
+  });
 })
