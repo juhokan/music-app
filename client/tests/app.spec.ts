5328d78a89314a1ebf64e430648895fb6e2f0a3f
Juho Kangas <juho.kangas@aalto.fi>
2024-07-16 18:23:09 +0300

    fix scripts

--- client/tests/app.spec.ts
+++ client/tests/app.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Music app', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('')
     await expect(page.locator('text=music review app')).toBeVisible()
   })
 })
