729160c27f4e8a7b15f8fc5e0c35158fdb58b29a
Juho Kangas <juho.kangas@aalto.fi>
2024-07-16 19:49:35 +0300

    different landing page

--- client/src/components/AppContainer.tsx
+++ client/src/components/AppContainer.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AppRoute } from '../routes'
-import { SearchContext, UserContext } from '../context'
+import { SearchContext } from '../context'
 import LandingPage from './HomePage'
-import LoginForm from './LoginPage/LoginForm'
 import Callback from '../spotify/Callback'
 import AlbumPage from './AlbumPage'
 import SearchPage from './SearchPage'
@@ -12,7 +11,6 @@ import ProfilePage from './ProfilePage'
 import Footer from './common/Footer'
 
 const AppContainer: React.FC = () => {
-  const { user } = React.useContext(UserContext)
   const { input } = React.useContext(SearchContext)
 
   return (
@@ -21,7 +19,7 @@ const AppContainer: React.FC = () => {
       <div id='main'>
         <Routes>
           <Route path={AppRoute.Home}>
-            {user ? (<Route index element={<LandingPage />} />) : <Route index element={<LoginForm />} />}
+            <Route index element={<LandingPage />} />
             <Route path=':albumId' element={<AlbumPage />} />
           </Route>
           <Route path={AppRoute.Callback}>
