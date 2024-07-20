1cd2b12e6662e9910f41d56fc3b0d05b652ff197
Juho Kangas <juho.kangas@aalto.fi>
2024-07-20 14:55:23 +0300

    react helmet

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>Music App</title>
+        <meta name='Music App' content='Review your music' />
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 )
