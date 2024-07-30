4c051f1d065d19c69b7f1e21c3a30a54a8998291
Juho Kangas <juho.kangas@aalto.fi>
2024-07-30 12:53:29 +0300

    responsive design for album grid

--- client/src/components/NewReleasesPage/index.tsx
+++ client/src/components/NewReleasesPage/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react'
 import { getNewReleases } from '../../services/spotifyService'
 import { AlbumData, SpotifyAlbumData } from '../../types'
--- client/src/components/common/AlbumsCarousel.tsx
+++ client/src/components/common/AlbumsCarousel.tsx
@@ -14,8 +14,23 @@ interface AlbumCarouselProps {
 
 const AlbumsCarousel: React.FC<AlbumCarouselProps> = ({ albums, header, route }) => {
   const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+    lgDesktop: {
+      breakpoint: { max: 3500, min: 2560 },
+      items: 6,
+      partialVisibilityGutter: 30
+    },
+    mdDesktop: {
+      breakpoint: { max: 2560, min: 1920 },
+      items: 5,
+      partialVisibilityGutter: 30
+    },
+    smDesktop: {
+      breakpoint: { max: 1920, min: 1280 },
+      items: 4,
+      partialVisibilityGutter: 30
+    },
+    xsDesktop: {
+      breakpoint: { max: 1280, min: 1024 },
       items: 3,
       partialVisibilityGutter: 30
     },
